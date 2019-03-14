f1999679fc47271f82f052e44db6df3417ccfedf
kiyashko <kiyashkolive@gmail.com>
2019-03-15 00:04:17 +0200

    14.03

--- news/views.py
+++ news/views.py
@@ -42,7 +42,6 @@ class PostDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = CommentForm()
         return context
 
     """Добавление комментариев"""
